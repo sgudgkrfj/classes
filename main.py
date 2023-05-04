5ac05536dec6a3a2fb8a5800e775f484016bbeb3
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-05-04 20:17:28 +0300

    Повністю матераіл

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ class Student:
     def __init__(self,height=150):
         self.height=height
         Student.count+=1
+    def breathing(self):
+        return self.height-10
 oleg=Student() #об'єкт, екземпляр класу
 print(oleg.height)
 masha=Student(height=200)
 print(masha.height)
 print(Student.count)
+print(masha.breathing())
