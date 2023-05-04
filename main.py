dc47c494e1262cbf067dbc592307f634e1825140
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-05-04 19:17:46 +0300

    classes_1Перший приклад атрибутів

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+#Classes
+class Student:
+    print("Hi!")
+
+    def __init__(self,height=150):
+        self.height=height
+oleg=Student() #об'єкт, екземпляр класу
+print(oleg.height)
+masha=Student(height=200)
+print(masha.height)
 
