803a7afc39b76fa20df0053a3dee1847967351e9
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-05-04 19:21:25 +0300

    підрахунок екземплярів класів

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #Classes
 class Student:
     print("Hi!")
-
+    count=0
     def __init__(self,height=150):
         self.height=height
+        Student.count+=1
 oleg=Student() #об'єкт, екземпляр класу
 print(oleg.height)
 masha=Student(height=200)
 print(masha.height)
-
+print(Student.count)
